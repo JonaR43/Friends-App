ab540bef320dde98b16c5ea2caaa5d3f99cefb8e
Jonathan Reyes <111107328+JonaR43@users.noreply.github.com>
2024-07-22 18:14:44 -0500

    final changes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 
@@ -9,6 +10,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+#Server static files from dist folder under front end dir
+@app.route("/", defaults={"filename": ""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
+
+
+#api routes CRUD
 import routes
 with app.app_context():
     db.create_all()
